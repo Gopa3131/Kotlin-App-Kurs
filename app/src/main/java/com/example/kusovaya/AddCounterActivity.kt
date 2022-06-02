f70959b8fbe4390f15b96e651d10c1ed1e0d9b9c
sashka10 <sashka.333@yandex.ru>
2022-06-02 21:51:30 +0300

    +AddCounterActivity xml file +AddCounterActivity +AddCounter viewModel file

--- app/src/main/java/com/example/kusovaya/AddCounterActivity.kt
+++ app/src/main/java/com/example/kusovaya/AddCounterActivity.kt
@@ -2,10 +2,62 @@ package com.example.kusovaya
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.*
+import androidx.lifecycle.ViewModelProvider
+import com.example.kusovaya.dataBase.Counter
+import kotlinx.coroutines.InternalCoroutinesApi
 
 class AddCounterActivity : AppCompatActivity() {
+
+    @InternalCoroutinesApi
+    private lateinit var viewModel : AddCounterViewModel
+
+
+    @InternalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_counter)
+
+        viewModel = ViewModelProvider(this).get(AddCounterViewModel::class.java)
+
+        val addCounterButton : Button = findViewById(R.id.addCounterActivity_addCounterButton)
+        val counterTypeSpinner : Spinner = findViewById(R.id.addCounterActivity_spinner)
+
+        val counterTypeList : Array<String> = resources.getStringArray(R.array.counter_types)
+        val adapter : ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_spinner_item, counterTypeList)
+        counterTypeSpinner.adapter = adapter
+
+        addCounterButton.setOnClickListener {
+            insertToDatabase()
+        }
+    }
+
+    @InternalCoroutinesApi
+    private fun insertToDatabase() {
+
+        val nameEditText : EditText = findViewById(R.id.addCounterActivity_addName)
+        val placeEditText : EditText = findViewById(R.id.addCounterActivity_addPlace)
+        val serNumEditText : EditText = findViewById(R.id.addCounterActivity_addSerNumber)
+        val counterTypeSpinner : Spinner = findViewById(R.id.addCounterActivity_spinner)
+
+        val name : String = nameEditText.text.toString()
+        val place : String = placeEditText.text.toString()
+        val serNum : String = serNumEditText.text.toString()
+        val counterType : String = counterTypeSpinner.selectedItem.toString()
+
+        if (inputCheck(name, place, serNum)) {
+            val counter : Counter = Counter(0, name, counterType, place, serNum)
+            viewModel.addCounter(counter)
+            Toast.makeText(this, "Счетчик добавлен", Toast.LENGTH_LONG).show()
+        }
+        else {
+            Toast.makeText(this, "Заполните пустые поля", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun inputCheck(name: String, place: String, serNum: String): Boolean {
+        return !(TextUtils.isEmpty(name) || TextUtils.isEmpty(place) || TextUtils.isEmpty(serNum))
     }
 }
--- app/src/main/java/com/example/kusovaya/AddCounterViewModel.kt
+++ app/src/main/java/com/example/kusovaya/AddCounterViewModel.kt
@@ -1,23 +1,28 @@
-package com.example.kusovaya.dataBase
+package com.example.kusovaya
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
+import com.example.kusovaya.dataBase.AppDatabase
+import com.example.kusovaya.dataBase.Counter
+import com.example.kusovaya.dataBase.Repository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.launch
 
-//difference between ViewModel and AndroidViewModel is AndroidViewModel can use Application context
 @InternalCoroutinesApi
-class CountersViewModel(application: Application) : AndroidViewModel(application) {
-
-    private val readAllCounters: LiveData<List<Counter>>
+class AddCounterViewModel(application: Application) : AndroidViewModel(application)
+{
     private val repository : Repository
 
     init {
         val myDao = AppDatabase.getDatabase(application).getMyDao()
         repository = Repository(myDao)
-        readAllCounters = repository.readAllCounters
+    }
+
+    fun addCounter(counter: Counter){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.addCounter(counter)
+        }
     }
 }
